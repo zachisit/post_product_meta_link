9c1d7cfd3af0939731fb1f72a5828e9ddd3e2dae
Zachary Smith <info@zachis.it>
2017-11-28 00:51:32 -0500

    working on Post metabox

--- post_product_meta_link.php
+++ post_product_meta_link.php
@@ -8,3 +8,39 @@ Author: Zach Smith
 Author URI: https://www.zachis.it
 License: GPL2
 */
+
+/**
+ * Register meta box
+ */
+function post_product_link_register_meta_boxes() {
+    add_meta_box( 'post_product_link_meta', __( 'Related Product', 'textdomain' ), 'post_product_link_display_callback', 'post', 'side', 'low' );
+}
+add_action( 'add_meta_boxes', 'post_product_link_register_meta_boxes' );
+
+function post_product_link_display_callback($post) {
+    echo '<p>Select from the below dropdown of Products to tie the Product to this Post. This will show the Product information on the sidebar of the Post on the frontend</p>';
+
+    //returnAllWooCommProducts();
+}
+
+function returnAllWooCommProducts() {
+    $args = [
+        'post_type' => [
+            'product',
+            'product_variation'
+            ],
+        'posts_per_page' => -1,
+        'order' => 'ASC'
+    ];
+
+    $wp_query = new WP_Query($args);
+
+    while ($wp_query->have_posts()) {
+        echo the_title();
+    }
+    //sort alpha
+    //sort($full_product_list);
+
+    //return $full_product_list;
+
+}
