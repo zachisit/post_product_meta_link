1edf51ad30d235e29aad070890d5c1eafd234da0
Zachary Smith <info@zachis.it>
2017-11-28 00:30:25 -0500

    introducing base plugin

---
+++ post_product_meta_link.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Related Post Product
+Plugin URI: https://www.zachis.it
+Description: Allows admin to select a Product when editing a Post that will show on the Post's sidebar
+Version: 1.0.1
+Author: Zach Smith
+Author URI: https://www.zachis.it
+License: GPL2
+*/
